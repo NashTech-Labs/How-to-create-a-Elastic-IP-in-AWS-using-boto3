e000a75da71306161842908423c7859ad8ebe919
MuZakkir Saifi <mohd.saifi@knoldus.com>
2022-08-27 16:48:44 +0530

    Update in code

--- create_elastic_ip.py
+++ create_elastic_ip.py
@@ -1,24 +1,26 @@
 # import the boto3 which will use to interact  with the aws
 import boto3
 
-AWS_REGION = input("Enter the AWS_REGION Name")
-EC2_CLIENT = boto3.client('ec2', region_name=AWS_REGION)
-
-allocation = EC2_CLIENT.allocate_address(
-    Domain='vpc',
+REGION = input("Please enter the your REGION Name: ")
+clientForIP = boto3.client('ec2', region_name=REGION)
+domain_name = input("Please enter the your domain Name: ")
+key = input("Please enter key value: ")
+key_value = input("Please enter key value: ")
+create = clientForIP.allocate_address(
+    Domain=domain_name,
     TagSpecifications=[
         {
             'ResourceType': 'elastic-ip',
             'Tags': [
                 {
-                    'Key': 'Name',
-                    'Value': 'tech_hub-elastic-ip'
+                    'Key': key,
+                    'Value': key_value
                 },
             ]
         },
     ]
 )
 
-print(f'It is Done, Your allocation ID is {allocation["AllocationId"]}')
-print(f'  - Wow, Your elastic IP {allocation["PublicIp"]} has been allocated')
+print(f'It is Done, Your allocation ID is {create["AllocationId"]}')
+print(f'  - Wow, Your elastic IP {create["PublicIp"]} has been allocated')
 print("Now you can use the IP")
